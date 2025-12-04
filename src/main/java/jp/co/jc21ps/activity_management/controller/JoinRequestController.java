0a8061b8b697c9aaa76267fe43f21d02541d8527
230514 <230514@jc-21.jp>
2025-12-04 11:17:32 +0900

    エラー修正？

--- src/main/java/jp/co/jc21ps/activity_management/controller/JoinRequestController.java
+++ src/main/java/jp/co/jc21ps/activity_management/controller/JoinRequestController.java
@@ -44,8 +44,18 @@ public class JoinRequestController {
 
         // セッションからuserIdを取得
         SessionDto sessionDto = commonService.getSessionDto(session);
-        String userId = sessionDto.getUserId();
-        String leaderClubId = sessionDto.getClubId();
+        //SessionDto sessionDto = commonService.getSessionDto(session);
+        //String userId = sessionDto.getUserId();
+        //String leaderClubId = sessionDto.getClubId();
+        String userId;
+        String leaderClubId;
+        if (sessionDto != null) {
+            userId = sessionDto.getUserId();
+            leaderClubId = sessionDto.getClubId();
+        } else {
+            userId = "";
+            leaderClubId = "";
+        }
 
         // セッションが切れた場合、エラー画面に遷移
         if (userId.isEmpty()) {
@@ -79,15 +89,11 @@ public class JoinRequestController {
         // リダイレクトされてきた登録申請成功のメッセージを、paramFormにセットする
         paramForm.setMessage(joinRequestCompleteMessage);
         
-        /*
-         * 初期表示情報取得結果に応じて、以下の条件文を完成させる。
-         */
         // リダイレクトされてきた登録申請成功のメッセージを、mavに追加
         if (joinRequestCompleteMessage != null && !joinRequestCompleteMessage.isEmpty()) {
             mav.addObject("joinRequestCompleteMessage", joinRequestCompleteMessage);
         }
-        //joinRequestList が空の場合: messages.properties から notRequestClubMessage を取得し、mav に追加
-        //joinRequestList が空でない場合: responseForm を mav.addObject("joinRequestSaveForm", responseForm) で追加
+
         // 初期表示情報取得結果に応じて、条件分岐
         if (joinRequestList.isEmpty()) {
             // 申請する部署が存在しない場合、メッセージを表示
@@ -130,18 +136,13 @@ public class JoinRequestController {
 
         try {
             boolean result = joinRequestService.insertJoinRequest(joinRequestSaveDto);
-            /*
-             * インサートの成功、失敗に応じて、処理を変更する。
-             */
+            
             // インサートの成功、失敗に応じて、処理を変更
             if (result) {
                 // インサート成功時、成功メッセージをリダイレクト属性に追加し、部員登録申請画面にリダイレクト
                 String joinRequestCompleteMessage = messageSource.getMessage("joinRequestCompleteMessage", null, Locale.getDefault());
                 redirectAttributes.addFlashAttribute("joinRequestCompleteMessage", joinRequestCompleteMessage);
                 mav.setViewName("redirect:/joinRequest");
-                //messages.properties から joinRequestCompleteMessage を取得
-                //redirectAttributes.addFlashAttribute でリダイレクト属性に追加
-                //redirect:/joinRequest にリダイレクト
             } else {
                 // インサート失敗時、エラー画面に遷移
                 mav.setViewName("error");
